8644d77ea3358b760c9438ae7fb5420d97a6bb7e
Robotiaga <sofartem5@gmail.com>
2023-05-24 20:45:45 +0500

    add function all_word_title

--- str_func.py
+++ str_func.py
@@ -1,7 +1,23 @@
-
 def all_title(string):
-    new_string =''
+    """
+    вся строка заглавными символами
+    :param string:
+    :return:
+    """
+    new_string = ''
     for i in string:
-        new_string+= i.title()
+        new_string += i.title()
     return new_string
-print(all_title('колбаса'))
+
+
+def all_word_title(string):
+    """
+    каждое слово начинается с заглавной
+    :param string:
+    :return:
+    """
+    new_string = ''
+    for i in string.split(' '):
+        new_string += i.title()
+        new_string += ' '
+    return new_string
