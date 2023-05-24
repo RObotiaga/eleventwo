61568e47108cde95438bb36cc13e081e38e6edc7
RObotiaga <44903919+RObotiaga@users.noreply.github.com>
2023-05-24 18:42:08 +0500

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,7 +1,9 @@
 
 def all_title(string):
+    """
+    делает все символы в строке заглавными
+    """
     new_string =''
     for i in string:
         new_string+= i.title()
     return new_string
-print(all_title('колбаса'))
